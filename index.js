66ae285affa65f11da0eac1ed84771cb67b64d26
tinnguyen <tinn1855@gmail.com>
2025-04-24 11:48:01 +0700

    deploy

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
-import connectDB from "./config/db.js";
-import todoRoutes from "./routes/todoRoutes.js";
 
 dotenv.config();
-connectDB();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
+app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+// MongoDB connect
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("✅ Connected to MongoDB"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
 
+// Routes
+import todoRoutes from "./routes/todoRoutes.js";
 app.use("/api/todos", todoRoutes);
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
-});
+// Start server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`🚀 Server is running on port ${PORT}`));
